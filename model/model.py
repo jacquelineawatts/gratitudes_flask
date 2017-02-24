84aa5f267d6a03c516c611a4987fb71095e2d267
Jacqueline Watts <jacquelineawatts@gmail.com>
2017-02-23 20:09:52 -0800

    Shifted class files into model folder

--- model/model.py
+++ model/model.py
@@ -1,4 +1,5 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from server import app
 
 db = SQLAlchemy()
 
@@ -7,12 +8,12 @@ def connect_to_db(app):
 
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///gratitudes"
     app.config['SQLALCHEMY_ECHO'] = True
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     db.app = app
     db.init_app(app)
 
 
 if __name__ == "__main__":
 
-    from server import app
     connect_to_db(app)
     print "Connected to db."
--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, render_template, session, flash, redirect, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
-from model import connect_to_db
-from user import User
-from entry import Entry, Gratitude
+from model.model import connect_to_db
+from model.user import User
+from model.entry import Entry, Gratitude
 import datetime
 
 app = Flask(__name__)
@@ -82,7 +82,7 @@ def process_login():
     """
 
     username = request.form.get('username')
-    user = User.get_by_username(username)
+    user = User.get_by_username(username) 
 
     if user:
         if request.form.get('password') == user.password:
@@ -107,7 +107,7 @@ def logout():
 # ----------------------------- RUN THESE FIRST --------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
     connect_to_db(app)
     DebugToolbarExtension(app)
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
