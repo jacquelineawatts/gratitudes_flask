7cdfe122b12a81fd0144c121e59daac6a74e3dc2
Jacqueline Watts <jacquelineawatts@gmail.com>
2017-02-27 08:59:49 -0800

    Incorporated connections class to allow follower-following relationships.

--- model/user.py
+++ model/user.py
@@ -1,6 +1,7 @@
 from model.model import connect_to_db, db
 from datetime import datetime
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+from flask import flash
 
 
 class User(db.Model):
@@ -17,6 +18,13 @@ class User(db.Model):
     location = db.Column(db.String(64), nullable=True)
     profile_image = db.Column(db.Text, nullable=True)
     date_joined = db.Column(db.DateTime, nullable=False)
+
+    following = db.relationship('User',
+                                secondary='connections',
+                                primaryjoin='Connection.follower_user_id == User.user_id',
+                                secondaryjoin='Connection.following_user_id == User.user_id',
+                                backref=db.backref('followers'),
+                                )
 
     def __repr__(self):
         """For a more manageable way to print object's attributes."""
@@ -57,23 +65,73 @@ class User(db.Model):
 
         return user
 
+    # WHERE DOES THIS ACTUALLY BELONG?
+    def handle_signup_form_errors(cls, first_name, last_name, username, email, password, password_confirmed, location, profile_image):
+        """Generates flash msgs depending on errors"""
 
-def handle_signup_form_errors(cls, first_name, last_name, username, email, password, password_confirmed, location, profile_image):
-    """Generates flash msgs depending on errors"""
+        errors = []
 
-    errors = []
+        if first_name or last_name or username or email or password:
+            pass
+        else:
+            errors.append("Your profile is incomplete.")
+            if password != password_confirmed:
+                errors.append("The passwords you've entered don't match.")
 
-    if first_name or last_name or username or email or password:
-        pass
-    else:
-        errors.append("Your profile is incomplete.")
-        if password != password_confirmed:
-            errors.append("The passwords you've entered don't match.")
+            elif username in users:
+                errors.append("I'm sorry, it looks like that username is taken already. Please select a new one.")
 
-        elif username in users:
-            errors.append("I'm sorry, it looks like that username is taken already. Please select a new one.")
+        return errors
 
-    return errors
+
+class Connection(db.Model):
+    """Class for Connections; like an association table for Users to Users relationships?"""
+
+    __tablename__ = "connections"
+
+    connection_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    follower_user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    following_user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    def __repr__(self):
+        return "<Connection ID: {}, Follower: {}, Following: {}>".format(self.connection_id,
+                                                                         self.follower_user_id,
+                                                                         self.following_user_id,
+                                                                         )
+
+    @classmethod
+    def add_connection_to_db(cls, follower_user_id, following_user_id):
+        """Given the follower and following user ids, adds connection to db."""
+
+        try:
+            connection = Connection.query.filter_by(follower_user_id=follower_user_id,
+                                                    following_user_id=following_user_id,
+                                                    ).one()
+            flash("You're already following that user!")
+
+        except NoResultFound:
+            connection = Connection(follower_user_id=follower_user_id,
+                                    following_user_id=following_user_id,
+                                    )
+            db.session.add(connection)
+            db.session.commit()
+            print "Added new connection object to the db."
+            following = User.query.get(following_user_id)
+            flash("You're now following {} {}".format(following.first_name, following.last_name))
+
+    @classmethod
+    def delete_connection(cls, follower_user_id, following_user_id):
+        """Delete a connection between users."""
+
+        try:
+            connection = Connection.query.filter_by(follower_user_id=follower_user_id,
+                                                    following_user_id=following_user_id,
+                                                    ).one()
+            db.session.delete(connection)
+            db.session.commit()
+
+        except NoResultFound:
+            print "That connection was not in the db."
 
 
 if __name__ == "__main__":
